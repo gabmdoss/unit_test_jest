3e4101e915a9ac14f8223863a43a0d7a51f0b1c3
Gabriela Mendes <gabmdoss@gmail.com>
2024-02-07 15:47:47 -0300

    SUM mini project assertions

--- sum.test.js
+++ sum.test.js
@@ -4,5 +4,10 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(5, 5)).toBe(10);
 });
 
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(5, 5)).not.toBe(12);
+});
   
-
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(5, 5)).toBeDefined();
+});
